77df22ef5cb3536f8397dc7af17b07572ae19365
Douglas Rocha <79188161+dougrocha@users.noreply.github.com>
2023-11-14 16:05:54 -0700

    fixed all formatting issues

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex min-h-screen w-full items-center justify-center">
+	<h1>Omen Fans</h1>
+</div>
